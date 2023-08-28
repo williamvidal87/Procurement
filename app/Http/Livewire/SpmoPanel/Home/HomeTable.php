748b9f2bfd57c259f883a798771a4bbbf9c54c1c
williamvidal87 <williamvidal09@yahoo.com>
2023-08-28 12:33:26 +0800

    make change

--- app/Http/Livewire/SpmoPanel/Home/HomeTable.php
+++ app/Http/Livewire/SpmoPanel/Home/HomeTable.php
@@ -7,6 +7,12 @@ use App\Models\InsertProcured;
 use App\Models\PurchaseRequestItem;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Infobip\Api\SmsApi;
+use Infobip\Configuration;
+use Infobip\Model\SmsAdvancedTextualRequest;
+use Infobip\Model\SmsDestination;
+use Infobip\Model\SmsTextualMessage;
+use Throwable;
 
 class HomeTable extends Component
 {
@@ -43,20 +49,40 @@ class HomeTable extends Component
     
     public function createPurchaseRequest($item_category_id,$quarter_id,$total_approve_budget)
     {
-        // $basic  = new \Vonage\Client\Credentials\Basic("e84d79f8", "EmUbN8mSqDbjfiq4");
-        // $client = new \Vonage\Client($basic);
+    
+        $BASE_URL = env('BASE_URL');
+        $API_KEY = env('API_KEY');
         
-        // $response = $client->sms()->send(
-        //     new \Vonage\SMS\Message\SMS("639973613510", "Procurement", 'sample notify')
-        // );
+        $SENDER = "InfoSMS";
+        $RECIPIENT = "63".Auth::user()->phone_number;
+        $MESSAGE_TEXT = "Sample Procurment";
         
-        // $message = $response->current();
+        $configuration = new Configuration(host: $BASE_URL, apiKey: $API_KEY);
         
-        // if ($message->getStatus() == 0) {
-        //     dd( "The message was sent successfully\n");
-        // } else {
-        //     dd( "The message failed with status: " . $message->getStatus() . "\n");
-        // }
+        $sendSmsApi = new SmsApi(config: $configuration);
+        
+        $destination = new SmsDestination(
+            to: $RECIPIENT
+        );
+        
+        $message = new SmsTextualMessage(destinations: [$destination], from: $SENDER, text: $MESSAGE_TEXT);
+        
+        $request = new SmsAdvancedTextualRequest(messages: [$message]);
+        
+        try {
+            $smsResponse = $sendSmsApi->sendSmsMessage($request);
+        
+            echo $smsResponse->getBulkId() . PHP_EOL;
+        
+            foreach ($smsResponse->getMessages() ?? [] as $message) {
+                echo sprintf('Message ID: %s, status: %s', $message->getMessageId(), $message->getStatus()?->getName()) . PHP_EOL;
+            }
+        } catch (Throwable $apiException) {
+            echo("HTTP Code: " . $apiException->getCode() . "\n");
+        }
+    
+    
+    
     
         $this->emit('openPurchaseRequestModal');
         $this->emit('ItemCategoryId',$item_category_id,$quarter_id,$total_approve_budget);
